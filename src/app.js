c957a9f8c83943444aa009c2c5c179c727ece8f5
sbaldioceda <sbaldioceda@gmail.com>
2021-04-02 16:44:37 +0000

    Cambios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var arreglo = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let valor = Math.floor(Math.random() * arreglo.length);
+
+  let numero = arreglo[valor];
+
+  var simbolo = ["♥", "♠", "♣", "♦"];
+  let simbolonumero = Math.floor(Math.random() * simbolo.length);
+  let NuevoSimbolo = simbolo[simbolonumero];
+
+  let cambiarNumero = document.getElementById("sizeNumber");
+  cambiarNumero.innerHTML = numero;
+  let simboloencabezado = document.getElementById("symbol-top");
+  let simbolopie = document.getElementById("symbol-botton");
+
+  if (NuevoSimbolo == "♠" || NuevoSimbolo == "♣") {
+    simboloencabezado.style.color = "black";
+    simbolopie.style.color = "black";
+  } else {
+    //corazones o diamantes
+
+    simboloencabezado.style.color = "red";
+    simbolopie.style.color = "red";
+  }
+  simboloencabezado.innerHTML = NuevoSimbolo;
+  simbolopie.innerHTML = NuevoSimbolo;
 };
